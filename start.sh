7e8ff659fd044c1177bc3bb96789eeb7bc561fe6
Amirul Islam <amirulandalib9@gmail.com>
2021-11-05 17:49:27 +0600

    finalise 0.2.6 master

--- start.sh
+++ start.sh
@@ -13,16 +13,14 @@ echo "ttk-x engine starting now --------"
 echo "
 
 THANK YOU FOR DEPLOYING }
-  ______          ______            ____   _ __ _  __
- /_  __/___  ____/_  __/___  ____  / / /__(_) /| |/ /
-  / / / __ \/ ___// / / __ \/ __ \/ / //_/ / __/   / 
- / / / /_/ / /   / / / /_/ / /_/ / / ,< / / /_/   |
-/_/  \____/_/   /_/  \____/\____/_/_/|_/_/\__/_/|_|  
 
+TOR TOOLKIT X
 
 Version - 0.2.6-Heroku-Master-Finalised.
 
-KANGED AND MODDED BY KANGERSHUB...... }
+CREATED BY - Yash Khadse
+
+KANGED AND MODDED BY KANGERSHUB.....
 
 "
 
